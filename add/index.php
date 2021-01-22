cc8d7cf10f1625504a0d2d45609b743261599092
Duong Tung Anh <duongtunganh2111@gmail.com>
2021-01-22 19:15:31 +0700

    added viewport and updated the sql file

--- add/index.php
+++ add/index.php
@@ -31,6 +31,7 @@ $conn->close();
 ?>
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Thêm bài viết - Everyday Tunna</title>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
